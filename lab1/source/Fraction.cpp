720d79e3d00a71d422d6bdf2736e6b10658f9b5d
Andrew Serbin <serbinadrian@outlook.com>
2024-09-17 22:53:02 +0300

    Added Fraction class and updated main

---
+++ lab1/source/Fraction.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+using namespace std;
+
+class Fraction
+{
+private:
+    int numerator;
+    int denominator;
+
+    const int gcd(int numerator, int denominator)
+    {
+        while (denominator != 0)
+        {
+            const int temp = denominator;
+            denominator = numerator % denominator;
+            numerator = temp;
+        }
+
+        return numerator;
+    }
+
+    void reduce()
+    {
+        const int divisor = gcd(this->numerator, this->denominator);
+
+        this->numerator /= divisor;
+        this->denominator /= divisor;
+
+        if (this->denominator < 0)
+        {
+            this->numerator = -this->numerator;
+            this->denominator = -this->denominator;
+        }
+    }
+
+public:
+    Fraction()
+    {
+        this->numerator = 0;
+        this->denominator = 1;
+    }
+
+    Fraction(int numerator, int denominator)
+    {
+        this->numerator = numerator;
+        this->denominator = denominator;
+
+        if (denominator == 0)
+        {
+            cerr << "Error: denominator cannot be zero! Using 1 instead..." << endl;
+            denominator = 1;
+        }
+
+        this->reduce();
+    }
+
+    Fraction operator+(const Fraction &fractionToAdd)
+    {
+        const int numerator = this->numerator * fractionToAdd.denominator + fractionToAdd.numerator * this->denominator;
+        const int denominator = this->denominator * fractionToAdd.denominator;
+
+        return Fraction(numerator, denominator);
+    }
+
+    Fraction operator-(const Fraction &fractionToSubtract)
+    {
+        const int numerator = this->numerator * fractionToSubtract.denominator - fractionToSubtract.numerator * this->denominator;
+        const int denominator = this->denominator * fractionToSubtract.denominator;
+
+        return Fraction(numerator, denominator);
+    }
+
+    Fraction operator*(const Fraction &fractionToMultiply)
+    {
+        const int numerator = this->numerator * fractionToMultiply.numerator;
+        const int denominator = this->denominator * fractionToMultiply.denominator;
+
+        return Fraction(numerator, denominator);
+    }
+
+    Fraction operator/(const Fraction &fractionToDivide)
+    {
+        if (fractionToDivide.numerator == 0)
+        {
+            cerr << "Error: division by zero!" << std::endl;
+            return Fraction(0, 1);
+        }
+        const int numerator = this->numerator * fractionToDivide.denominator;
+        const int denominator = this->denominator * fractionToDivide.numerator;
+
+        return Fraction(numerator, denominator);
+    }
+
+    bool operator==(const Fraction &otherFraction)
+    {
+        return this->numerator == otherFraction.numerator && this->denominator == otherFraction.denominator;
+    }
+
+    bool operator!=(const Fraction &otherFraction)
+    {
+        return !(*this == otherFraction);
+    }
+
+    bool operator<(const Fraction &otherFraction)
+    {
+        return this->numerator * otherFraction.denominator < otherFraction.numerator * this->denominator;
+    }
+
+    bool operator>(const Fraction &otherFraction) const
+    {
+        return this->numerator * otherFraction.denominator > otherFraction.numerator * this->denominator;
+    }
+
+    Fraction &operator=(const Fraction &otherFraction)
+    {
+        if (this == &otherFraction)
+        {
+            return *this;
+        }
+
+        this->numerator = otherFraction.numerator;
+        this->denominator = otherFraction.denominator;
+
+        this->reduce();
+
+        return *this;
+    }
+
+    friend ostream &operator<<(ostream &output, const Fraction &currentFraction)
+    {
+        output << currentFraction.numerator << "/" << currentFraction.denominator;
+        return output;
+    }
+
+    friend istream &operator>>(istream &input, Fraction &currentFraction)
+    {
+        int numerator = 0, denominator = 0;
+
+        cout << "Input numerator: ";
+        input >> numerator;
+
+        cout << "Input denominator: ";
+        input >> denominator;
+
+        currentFraction.numerator = numerator;
+        currentFraction.denominator = denominator;
+
+        return input;
+    }
+};
--- lab1/source/main.cpp
+++ lab1/source/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "String.cpp"
+#include "Fraction.cpp"
 using namespace std;
 
 int main()
@@ -15,7 +16,7 @@ int main()
 
     cout << "Please input test string: ";
     cin >> string6;
-   
+
     cout << "string1 (char *stringToCopy): " << string1 << endl;
     cout << "string2 (char *stringToCopy): " << string2 << endl;
     cout << "string3 (string1 + \" \" + string2): " << string3 << endl;
@@ -28,7 +29,32 @@ int main()
     string4.insert(string6, 6);
 
     cout << "remove World! and insert string6 at position 6: " << string4 << endl;
-    cout << "size of string4: " << string4.getSize() << endl;
+    cout << "size of string4: " << string4.getSize() << endl
+         << endl;
+
+    system("pause");
+
+    Fraction fraction1(1, 2); // 1/2
+    Fraction fraction2(3, 4); // 3/4
+    Fraction fraction3;
+
+    cin >> fraction3;
+
+    cout << "fraction1: " << fraction1 << endl;
+    cout << "fraction2: " << fraction2 << endl;
+    cout << "fraction3(from cin): " << fraction3 << endl;
+
+    cout << "fraction1 + fraction2: " << fraction1 + fraction2 << endl;
+    cout << "fraction1 - fraction2: " << fraction1 - fraction2 << endl;
+    cout << "fraction1 * fraction2: " << fraction1 * fraction2 << endl;
+    cout << "fraction1 / fraction2: " << fraction1 / fraction2 << endl;
+
+    cout << "fraction1 < fraction2: " << (fraction1 < fraction2 ? "YES" : "NO") << endl;
+    cout << "fraction1 > fraction2: " << (fraction1 > fraction2 ? "YES" : "NO") << endl;
+    cout << "fraction1 != fraction2: " << (fraction1 != fraction2 ? "YES" : "NO") << endl;
+    cout << "fraction1 == fraction3: " << (fraction1 == fraction3 ? "YES" : "NO") << endl
+         << endl;
+
     system("pause");
 
     return 0;
